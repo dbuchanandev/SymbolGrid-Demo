5c0479ffa763beff58d0423295cb90a6dcc16732
Donavon Buchanan <5569820+donavoncade@users.noreply.github.com>
2020-09-12 02:28:57 -0400

    Initial Commit

---
+++ Shared/SymbolGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolGridApp.swift
+//  Shared
+//
+//  Created by Donavon Buchanan on 9/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SymbolGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
